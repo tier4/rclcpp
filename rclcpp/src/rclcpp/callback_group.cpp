cfcc8f08146917545db2f9acf7204c1d8a44c18c
atsushi421 <atsushi.yano.2@tier4.jp>
2026-02-22 10:51:48 +0900

    docs: add comment

--- rclcpp/src/rclcpp/callback_group.cpp
+++ rclcpp/src/rclcpp/callback_group.cpp
@@ -28,7 +28,12 @@ CallbackGroup::CallbackGroup(
 {}
 
 CallbackGroup::~CallbackGroup()
-{}
+{
+  // NOTE: upstream rclcpp calls trigger_notify_guard_condition() here to notify the
+  // executor that this callback group is being destroyed. t4-main does not have the
+  // notify_guard_condition_ member or trigger_notify_guard_condition() method in CallbackGroup,
+  // so the destructor body is intentionally empty for now.
+}
 
 std::atomic_bool &
 CallbackGroup::can_be_taken_from()
